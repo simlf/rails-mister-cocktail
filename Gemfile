0fc5305b48562262b834e4fe1d90ed35e3e524a2
Simon Le Fourn <lefournsimon@gmail.com>
2019-05-31 15:38:01 +0200

    added cloudinary key to clever

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.5.3'
 
-
+gem 'carrierwave', '~> 1.2'
 gem 'cloudinary', '~> 1.9.1'
 gem 'dotenv-rails', groups: [:development, :test]
 gem 'autoprefixer-rails'
--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,8 +1,11 @@
 class Cocktail < ApplicationRecord
+  mount_uploader :photo, PhotoUploader
   has_many :doses, dependent: :destroy
 
   has_many :ingredients, through: :doses
 
   validates :name, presence: true
   validates :name, uniqueness: true
+
 end
+
